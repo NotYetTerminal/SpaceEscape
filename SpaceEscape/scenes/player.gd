62cd01c79cad6cc1acddb5f49aee316d2792b449
532stary4 <61381495+NotYetTerminal@users.noreply.github.com>
2023-07-05 12:33:47 +0100

    upadate

---
+++ SpaceEscape/scenes/player.gd
@@ -0,0 +1,35 @@
+extends Area2D
+
+signal hit
+
+@export var speed: float = 300
+@export var sprite_size: Vector2 = Vector2(128, 128)
+var screen_size: Vector2
+
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	start(Vector2(100, screen_size.y / 2))
+
+
+func _process(delta: float) -> void:
+	var velocity: float = 0
+	if Input.is_action_pressed("move_down"):
+		velocity += speed
+	if Input.is_action_pressed("move_up"):
+		velocity -= speed
+	
+	position.y += velocity * delta
+	position.y = clamp(position.y, sprite_size.y / 2, screen_size.y - sprite_size.y / 2)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+
+
+func start(pos: Vector2) -> void:
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
